db338c38e8828406d1ad2cf8d7ef8a634c02ad38
Amir Shaikh <iamirshaikh02.com>
2023-10-28 22:59:19 +0530

    searching functionality added

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,8 +1,9 @@
 
 import React, { useEffect, useReducer } from 'react';
 import Thumbnails from '../../components/Thumbnails/Thumbnails';
-import { getAll } from '../../services/foodService';
-
+import { getAll} from '../../services/foodService';
+import { useParams } from 'react-router-dom';
+import Search from '../../components/Search/Search';
 
 const initialState = { foods: [] };
 
@@ -18,13 +19,20 @@ const reducer = (state, action) => {
 export default function HomePage() {
     const [state, dispatch] = useReducer(reducer, initialState);
     const { foods } = state;
+    const { searchTerm } = useParams();
   
     useEffect(() => {
       getAll().then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }));
     }, []);
-
+    //   const loadFoods = searchTerm ? search(searchTerm) : getAll();
+  
+    //   loadFoods.then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }));
+    // }, [searchTerm]);
+      
   return (
     <div>
+      <Search />
+
        <Thumbnails foods={foods} />
     </div>
   );
